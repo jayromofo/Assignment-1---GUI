488aee1f0a409a10fdd27ac88436961f344a6744
jayromofo <jayromofo@outlook.com>
2021-10-15 19:06:07 -0400

    In working order, ready to merge

--- src/StudentFrame.java
+++ src/StudentFrame.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.InvalidMarkException;
 import java.util.ArrayList;
 
 /**
@@ -52,7 +53,7 @@ public class StudentFrame extends JFrame implements ActionListener {
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // ArrayList that holds all the students
-    // TODO: Find a better place for this
+    // TODO: Find a better place for this??
     final static ArrayList<Student> studentList = new ArrayList<>();
     static int currentIndex = 0;
 
@@ -62,8 +63,6 @@ public class StudentFrame extends JFrame implements ActionListener {
         generateFields();
         // Startup state for text boxes and buttons
         setInitialState();
-
-
         ///////////////////////////////////////////////////////////
         // ACTION LISTENERS
         ///////////////////////////////////////////////////////////
@@ -111,8 +110,7 @@ public class StudentFrame extends JFrame implements ActionListener {
                     btnEdit.setText("Edit");
                     btnAdd.setEnabled(true);
                     btnLoad.setEnabled(true);
-
-
+                    btnNext.setEnabled(false);
                 }
             });
         });
@@ -121,8 +119,6 @@ public class StudentFrame extends JFrame implements ActionListener {
         btnLoad.addActionListener(this);
         btnPrev.addActionListener(this);
         btnNext.addActionListener(this);
-
-
     } // End JFrame
 
     // Generate all the components of the frame
@@ -136,17 +132,14 @@ public class StudentFrame extends JFrame implements ActionListener {
         panFields.add(txtFirstName);
         panFields.add(lblLastName);
         panFields.add(txtLastName);
-
         // Add all the buttons on the bottom to the button panel
         panButtonRow.add(btnLoad);
         panButtonRow.add(btnEdit);
         panButtonRow.add(btnAdd);
         panButtonRow.add(btnSave);
-
         // Add the fields and button panels into the center panel
         panCenter.add(panFields, BorderLayout.CENTER);
         panCenter.add(panButtonRow, BorderLayout.SOUTH);
-
         // Add the label and marks to the mark area panel
         panMarkArea.add(lblMarks, BorderLayout.NORTH);
         // Try out array
@@ -167,13 +160,13 @@ public class StudentFrame extends JFrame implements ActionListener {
         panStudent.add(panCenter, BorderLayout.CENTER);
         panStudent.add(btnNext, BorderLayout.EAST);
         panStudent.add(panMarkArea, BorderLayout.SOUTH);
-
+        // MAGIC
         getContentPane().add(panStudent);
         invalidate(); validate();
 
     }
 
-    // Set the inital state for the program
+    // Set the initial state for the program
     private void setInitialState() {
         // Set the buttons and text fields
         btnLoad.setEnabled(true);
@@ -196,34 +189,26 @@ public class StudentFrame extends JFrame implements ActionListener {
     }
 
     // Update all the information to get the button states
+    // Use this after each event to keep it up to date
     public void update() {
         int length = studentList.size();
-        System.out.println("From Update\nIndex: "+currentIndex +"\tList Length: "+ studentList.size());
+        System.out.println("From Update\nIndex: "+currentIndex +"\tList Length: "+ studentList.size()); // DEBUG
         try
         {
             btnNext.setEnabled(true);
             btnPrev.setEnabled(true);
-            System.out.println("Index: "+currentIndex +"\tList - 1: "+ (length - 1));
-            // If StudentList is empty buttons disabled
+            System.out.println("Index: "+currentIndex +"\tList - 1: "+ (length - 1)); // DEBUG
+            // If StudentList is empty buttons are disabled START STATE
             if (studentList.isEmpty()) {
                 btnPrev.setEnabled(false);
                 btnNext.setEnabled(false);
             }
-
             // If index is 0 prev disabled
             if (currentIndex == 0) {
                 btnPrev.setEnabled(false);
             }  else if (currentIndex == (length - 1) || studentList.size() == 1){
                 btnNext.setEnabled(false);
             }
-
-
-
-            // If index ==  length - 1 next disable
-
-
-
-
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -231,12 +216,14 @@ public class StudentFrame extends JFrame implements ActionListener {
 
     }
 
+    // Clear all of the mark values
     private void clearMarks() {
         for (JTextField marks : txtMarks) {
             marks.setText("");
         }
     }
 
+    // Clear all the text boxes on the top
     private void clearText() {
         txtID.setText("");
         txtFirstName.setText("");
@@ -244,6 +231,7 @@ public class StudentFrame extends JFrame implements ActionListener {
         txtLastName.setText("");
     }
 
+    // Enables or disables the text boxes
     private void enableTextBoxes(boolean result) {
         txtFirstName.setEnabled(result);
         txtID.setEnabled(result);
@@ -254,82 +242,89 @@ public class StudentFrame extends JFrame implements ActionListener {
         }
     }
 
+    // Gets the text from the text boxes and sets it in the created student.
     public void createStudent(){
         Student currentStudent = studentList.get(studentList.size() - 1);
         String firstName = txtFirstName.getText();
         String lastName = txtLastName.getText();
         String program = txtProgram.getText();
-        double[] studentMarks = new double[6];
+        double[] studentMarks = currentStudent.getMarks();
+
         int markIndex = 0;
-
-        // If there are values in the marks then add them to the array
-        // If not set all marks to 0
-        if (studentMarks[0] != 0) {
-            for (JTextField mk : txtMarks) {
-                studentMarks[markIndex] = Double.parseDouble(mk.getText());
-                markIndex++;
+        for (JTextField mk : txtMarks) {
+            // If no marks are in the text boxes, set them to 0
+            if (mk.getText().isEmpty()){
+                //studentMarks[markIndex] = 0;
+                mk.setText(String.valueOf(0.0f));
             }
-
-        } else {
-            for (JTextField mk : txtMarks) {
-                studentMarks[markIndex] = 0;
-                markIndex++;
-            }
+            // If there are values in the marks then add them to the array
+            studentMarks[markIndex] = Double.parseDouble(mk.getText());
+            markIndex++;
         }
 
 
-        // Edit the student information
+
+        // Set the student information with the text box information
         currentStudent.setFname(firstName);
         currentStudent.setLname(lastName);
         currentStudent.setProgram(program);
         currentStudent.setMarks(studentMarks);
+        // Repositions the current index
         currentIndex = studentList.size() - 1;
         update();
     }
 
+    // Displays the current student with no access to the text boxes
     private void displayCurrentStudent(Student student) {
-
         enableTextBoxes(false);
     }
 
     public void actionPerformed(ActionEvent e) {
 
-        // TODO: Need to do some in range validation 0 < length
+        // When Next Button is clicked, it increases the index by 1 and loads that student.
         if (e.getSource() == btnPrev) {
             update();
             if (currentIndex > 0) {
                 currentIndex -= 1;
                 loadStudent(studentList.get(currentIndex));
-                System.out.println("Current Index: "+ currentIndex);
-                System.out.println("Next index from Prev is: "+ currentIndex);
+                System.out.println("Current Index: "+ currentIndex); // DEBUG
+                System.out.println("Next index from Prev is: "+ currentIndex); // DEBUG
             } else {
                 System.out.println("Out of range");
             }
 
         }
+        // When Next Button is clicked, it increases the index by 1 and loads that student.
         if (e.getSource() == btnNext) {
             update();
             if (currentIndex < studentList.size() - 1){
                 currentIndex += 1;
                 loadStudent(studentList.get(currentIndex));
-                System.out.println("Current Index: "+ currentIndex);
-                System.out.println("Next index from Next is: "+ currentIndex);
+                System.out.println("Current Index: "+ currentIndex); // DEBUG
+                System.out.println("Next index from Next is: "+ currentIndex); // DEBUG
             } else {
                 System.out.println("Out of range");
             }
         }
     }
 
+    // Loads the current student into the text boxes
     public void loadStudent(Student student) {
         txtID.setText(student.getStudentID());
         txtProgram.setText(student.getProgram());
         txtFirstName.setText(student.getFname());
         txtLastName.setText(student.getLname());
-
+        // Counter for the list of marks
         int i = 0;
-        for (double mk : student.getMarks()){
-            txtMarks[i].setText(String.valueOf(mk));
+        // Set each mark textbox
+        for (double mark : student.getMarks()){
+            txtMarks[i].setText(String.valueOf(mark));
+            ++i;
         }
+        // DEBUG
+        double[] marks = student.getMarks(); // DEBUG
+        System.out.printf("LOADING STUDENT: %s %s %s %s", student.getStudentID(), student.getFname(), student.getLname(), student.getProgram()); // DEBUG
+        System.out.printf("GRADES: %2f %2f %2f %2f %2f %2f", marks[0], marks[1], marks[2], marks[3], marks[4], marks[5]); // DEBUG
         update();
     }
 
